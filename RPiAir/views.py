44d0b3decf02d6d00fa71955f2f2d060dd2035ed
Patrick (Magnus) <patrick@fake.server>
2013-08-25 15:16:46 +0200

    Better layout with every 4 thumbs in a seperate row

--- RPiAir/views.py
+++ RPiAir/views.py
@@ -1,4 +1,4 @@
-from flask import g, render_template, request
+from flask import render_template, request
 from RPiAir import app
 from RPiAir.omxplayer import player
 from RPiAir.database import database, Movie
@@ -11,18 +11,18 @@ database.create_all()
 #  views
 @app.route('/')
 def show_player():
-    recentMovies = Movie.query.order_by(Movie.added_on.desc()).limit(100).all()
+    recentMovies = Movie.query.order_by(Movie.added_on.desc()).limit(20).all()
     return render_template('player.html', movies=recentMovies, debug=app.config['DEBUG'])
 
 @app.route('/play')
 def omx_play():
     #  spawn new process and store in cache
-    f = request.args.get('filename')
-    return player.play_file(f)
+    filename = request.args.get('f')
+    return player.play_file(filename)
 
 @app.route('/run')
-def omx_pause():
-    cmd = request.args.get('command')
+def omx_run():
+    cmd = request.args.get('cmd')
     return player.run_command(cmd)
 
 @app.route('/rescan')
